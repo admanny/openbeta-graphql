3f23f3a96eca4e43b112086602c8a3248d33a953
viet nguyen <vietnguyen@noreply>
2022-07-13 16:39:53 +0200

    Debug auth 8

--- src/db/index.ts
+++ src/db/index.ts
@@ -21,7 +21,7 @@ const defaultFn = console.log.bind(console, 'DB connected successfully')
 
 export const connectDB = async (onConnected: () => any = defaultFn): Promise<any> => {
   const user = checkVar('MONGO_INITDB_ROOT_USERNAME')
-  // const pass = checkVar('MONGO_INITDB_ROOT_PASSWORD')
+  const pass = checkVar('MONGO_INITDB_ROOT_PASSWORD')
   const server = checkVar('MONGO_SERVICE')
 
   console.log(
@@ -37,11 +37,9 @@ export const connectDB = async (onConnected: () => any = defaultFn): Promise<any
         process.exit(1)
       }
     )
-    // authSource=admin
-    // `mongodb://${user}:${pass}@${server}:27017/opentacos?readPreference=primary&ssl=false&replicaSet=rs0`
     await mongoose.connect(
-    `mongodb://${server}:27017/opentacos?readPreference=primary&ssl=false&replicaSet=rs0`,
-    { autoIndex: false }
+      `mongodb://${user}:${pass}@${server}:27017/opentacos?authSource=admin&readPreference=primary&ssl=false&replicaSet=rs0`,
+      { autoIndex: false }
     )
   } catch (e) {
     console.error("Can't connect to db")
